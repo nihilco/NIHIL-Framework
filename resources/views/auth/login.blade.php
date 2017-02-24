455df9074dd1ec0ab3d3fa4a7ed198a169c79e8d
Uriah M. Clemmer IV <uriah@nihil.co>
2017-02-24 10:26:22 -0500

    Created login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,45 @@
-@extends('layouts.app')
+@extends('layouts.minimal')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                  <div class="panel-body">
+                     <p class="text-center pv">SIGN IN TO CONTINUE.</p>
+                     <form role="form" data-parsley-validate="" novalidate="" class="mb-lg" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+                           <label for="email" class="text-muted">Email</label>
+                           <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="jsmith@example.com" autocomplete="off" required autofocus>
+                           <span class="fa fa-envelope form-control-feedback text-muted"></span>
+                           @if ($errors->has('email'))
+                           <span class="help-block">
+                             <strong>{{ $errors->first('email') }}</strong>
+                           </span>
+                           @endif
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+                           <label for="password" class="text-muted">Password</label>
+                           <input id="password" type="password" class="form-control" name="password" placeholder="W0rdP@ss!" required>
+                           <span class="fa fa-lock form-control-feedback text-muted"></span>
+                           @if ($errors->has('password'))
+                           <span class="help-block">
+                             <strong>{{ $errors->first('password') }}</strong>
+                           </span>
+                           @endif
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
+                        <div class="clearfix">
+                           <div class="checkbox c-checkbox pull-left mt0">
+                              <label>
+                                 <input type="checkbox" name="remember" data-parsley-multiple="remember" {{ old('remember') ? 'checked' : '' }}>
+                                 <span class="fa fa-check"></span>Remember Me
+                              </label>
+                           </div>
+                           <div class="pull-right"><a href="{{ route('password.request') }}" class="text-muted">Forgot your password?</a>
+                           </div>
                         </div>
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-block btn-primary mt-lg">Login</button>
+                     </form>
+                     <p class="pt-lg text-center">Need to Signup?</p><a href="{{ route('register') }}" class="btn btn-block btn-default">Register Now</a>
+                  </div>
+               </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,57 @@
-@extends('layouts.app')
+@extends('layouts.minimal')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                   <p class="text-center pv">SIGNUP TO GET INSTANT ACCESS.</p>
+                   <form role="form" data-parsley-validate="" novalidate="" class="mb-lg" method="POST" action="{{ route('register') }}">
+                      {{ csrf_field() }}
+                      <div class="form-group has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
+                         <label for="name" class="text-muted">Name</label>
+                         <input id="name" type="text" class="form-control" name="name" placeholder="John Smith" autocomplete="off" value="{{ old('name') }}" required autofocus>
+                         <span class="fa fa-user form-control-feedback text-muted"></span>
+                         @if ($errors->has('name'))
+                         <span class="help-block">
+                            <strong>{{ $errors->first('name') }}</strong>
+                         </span>
+                         @endif
+                      </div>
+                      <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+                         <label for="email" class="text-muted">Email</label>
+                         <input id="email" type="email" class="form-control" name="email" placeholder="jsmith@example.com" autocomplete="off" value="{{ old('email') }}" required>
+                         <span class="fa fa-envelope form-control-feedback text-muted"></span>
+                         @if ($errors->has('email'))
+                         <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                         </span>
+                         @endif
+                      </div>
+                      <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+                         <label for="password" class="text-muted">Password</label>
+                         <input id="password" type="password" class="form-control" name="password" placeholder="W0rdP@ss!" required>
+                         <span class="fa fa-lock form-control-feedback text-muted"></span>
+                         @if ($errors->has('password'))
+                         <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                         </span>
+                         @endif
+                      </div>
+                      <div class="form-group has-feedback">
+                         <label for="password_confirmation" class="text-muted">Confirm Password</label>
+                         <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" placeholder="W0rdP@ss!" data-parsley-equalto="#password" required>
+                         <span class="fa fa-lock form-control-feedback text-muted"></span>
+                      </div>
+                      <div class="clearfix">
+                         <div class="checkbox c-checkbox pull-left mt0">
+                            <label>
+                               <input type="checkbox" name="agreed" data-parsley-multiple="agreed" {{ old('agreed') ? 'checked' : '' }}>
+                               <span class="fa fa-check"></span>I agree with the <a href="#">terms</a>
+                            </label>
+                         </div>
+                      </div>
+                      <button type="submit" class="btn btn-block btn-primary mt-lg">Create account</button>
+                   </form>
+                   <p class="pt-lg text-center">Have an account?</p><a href="{{ route('login') }}" class="btn btn-block btn-default">Login</a>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+             </div>
 @endsection
