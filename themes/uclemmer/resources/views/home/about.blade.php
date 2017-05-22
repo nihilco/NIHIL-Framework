e4f0667b7ac64e2700a56149594a3e882109a866
Uriah M. Clemmer IV <uriah@nihil.co>
2017-05-22 13:33:04 -0400

    Fixed content

--- themes/uclemmer/resources/views/home/about.blade.php
+++ themes/uclemmer/resources/views/home/about.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.master')
 
-<h1>About</h1>
-    
+@section('content')
+
+<section id="site-content">
+    <div class="container">
+
+    <div class="row">
+    <div class="col-sm-12">
+    <h1>About</h1>
+    <p>This site is under development.  More coming soon.</p>
+    </div>
+    </div>
+
+    </div>
+    </section>
+
 @endsection
