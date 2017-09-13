21f1228abcdc2f2adca888fb4f705e8eac1f223b
Uriah M. Clemmer IV <uriah@nihil.co>
2017-09-13 08:05:15 -0400

    Another comment

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        // New
+        // New Comment
         App\Providers\ConfigServiceProvider::class,
         App\Providers\TemplateServiceProvider::class,
         //
